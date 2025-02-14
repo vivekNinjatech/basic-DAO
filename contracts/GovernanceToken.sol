e57393409df7d49c165aeefc98e1834873e0f5cc
vivekNinjatech <vivek@ninjatechnolabs.com>
2025-02-14 15:24:32 +0530

    add: adding deploy governor token file fix: solving openzeppelin and ts errors (in-library import errors) (not solved)

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.2;
 
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
-import "@openzeppelin/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
-
+import "@openzeppelin/contracts/token/ERC20/extensions/draft/IERC20Permit.sol";
 contract GovernanceToken is ERC20Votes {
-    uint256 public s_maxSupply = 1000000000000000000000000;
+    uint256 public s_maxSupply = 1_000_000 * 10**18; // More readable format
 
     constructor()
         ERC20("GovernanceToken", "GT")
-        ERC20Permit("GovernanceToken")
+        ERC20Votes()
     {
         _mint(msg.sender, s_maxSupply);
     }
@@ -20,17 +19,17 @@ contract GovernanceToken is ERC20Votes {
         address from,
         address to,
         uint256 amount
-    ) internal override(ERC20Votes) {
+    ) internal override(ERC20) { // ✅ Override from ERC20, not ERC20Votes
         super._afterTokenTransfer(from, to, amount);
     }
 
-    function _mint(address to, uint256 amount) internal override(ERC20Votes) {
+    function _mint(address to, uint256 amount) internal override(ERC20) { // ✅ Override from ERC20
         super._mint(to, amount);
     }
 
     function _burn(address account, uint256 amount)
         internal
-        override(ERC20Votes)
+        override(ERC20) // ✅ Override from ERC20
     {
         super._burn(account, amount);
     }
---
+++ deploy/01-deploy-governor-token.ts
@@ -0,0 +1,8 @@
+import {HardhatRuntimeEnvironment} from "hardhat/types"
+import {DeployFunction} from 'hardhat-deploy/types';
+
+const deployGovernorToken: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  // code here
+  console.log("I ran...!")
+};
+export default deployGovernorToken;
